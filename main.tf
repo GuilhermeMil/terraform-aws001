ae731788664a8e55f707f305b303290e53b29a5c
Guilherme <ggmilesi@gmail.com>
2023-01-31 21:48:00 -0300

    BucketS3

---
+++ main.tf
@@ -0,0 +1,15 @@
+terraform  { 
+    required_providers {
+      aws = {
+        source = "hashicorp/aws"
+        version = "~> 4.16"
+      }
+    }
+    required_version = ">= 1.2.0"
+}
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "p001.terraform.s3"
+}
